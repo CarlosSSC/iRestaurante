a144258bc85bb846344c5a79c4381339d93cdce8
CarlosSSC <a6520150015@utch.edu.mx>
2023-06-19 14:28:35 -0600

    Components added and header fixed
    Header component created, shared modules stablished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from 'src/app/components/SharedModule';
 
 @NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule {}
--- src/app/pages/main-screen/main-screen.module.ts
+++ src/app/pages/main-screen/main-screen.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { MainScreenPageRoutingModule } from './main-screen-routing.module';
-
 import { MainScreenPage } from './main-screen.page';
-
+import { SharedModule } from 'src/app/components/SharedModule';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MainScreenPageRoutingModule
+    MainScreenPageRoutingModule,
+    SharedModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [MainScreenPage]
 })
 export class MainScreenPageModule {}
