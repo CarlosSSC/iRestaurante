c2b8b9e903e19fc78577e0602d89da612a1ab2c6
CarlosSSC <a6520150015@utch.edu.mx>
2023-06-29 22:57:22 -0600

    Improved the menu of the mainscreen
    titles, scrollable buttons for the carousel (provisional) and an always visible "view order" button was added

--- src/app/pages/main-screen/main-screen.page.ts
+++ src/app/pages/main-screen/main-screen.page.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-screen.page.scss'],
 })
 export class MainScreenPage implements OnInit {
+  buttons = [
+    { label: 'Button 1', isSelected: false },
+    { label: 'Button 2', isSelected: false},
+    { label: 'Button 3', isSelected: false},
+    { label: 'Button 1', isSelected: false },
+    { label: 'Button 2', isSelected: false },
+    { label: 'Button 3', isSelected: false },
+    { label: 'Button 1', isSelected: false },
+    { label: 'Button 2', isSelected: false },
+    { label: 'Button 3', isSelected: false },
+    { label: 'Button 1', isSelected: false },
+    { label: 'Button 2', isSelected: false },
+    { label: 'Button 3', isSelected: false },
+  ];
+
+  selectedButtonIndex: number | null = null;
+
+  selectButton(index: number) {
+    if (this.selectedButtonIndex === index) {
+      this.selectedButtonIndex = null; // Dehighlight the button if it's already selected
+    } else {
+      this.selectedButtonIndex = index; // Highlight the button if it's not selected
+    }
+  }
 
   constructor() { }
 
